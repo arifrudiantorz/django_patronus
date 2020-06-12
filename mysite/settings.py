0932b8989755b3defa36a8fe612504e32b9ba576
arifrudiantorz <arifrudiantoro48@gmail.com>
2020-06-12 13:45:49 +0700

    Instade

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -52,6 +53,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -129,7 +131,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 EMAIL_HOST = 'smtp.gmail.com'
@@ -154,3 +156,8 @@ REST_FRAMEWORK = {
     ]
 }
 
+django_heroku.settings(locals())
+
+import dj_database_url
+db_form_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_form_env)
